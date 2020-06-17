c2627843c085cb40cbdd0a3a288140e9b97938c4
KigulaJesse <Jkigula@icloud.com>
2020-06-18 02:16:10 +0300

    tired

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -17,6 +17,20 @@
 	<!-- Container End -->
 </section>
 
+<style>
+    .parent{
+        position: relative;
+    }
+    .divEl{
+        height: 100px;
+        width: 100px;
+        position: absolute;
+        left: 50%;
+        margin-left: -50px;
+        top: 50%;
+        margin-top: -50px;
+    }
+</style>
 <section class="dashboard section">
     <!-- Container Start -->
     <div class="container">
@@ -24,96 +38,96 @@
         <div class="row">
         
             <div class="col-lg-9 col-md-1">
-                <div class="widget dashboard-container my-adslist col-lg-20" @if(count($products)== 0)style = "position:relative; left:100px;"@endif>
-                    <table class="table table-responsive product-dashboard-table" style = "overflow: visible;" >
-                        @if(count($products)>0)
-                        <thead>
-                            <tr>
-                                <th>image</th>
-                                <th>Product </th>
-                                <th class = "text-center">Price</th>
-                                <th class = "text-center">Qty</th>
-                                <th class = "text-center">SubTotal</th>
-                                <th class = "text-center">Action</th>
+                <div class="widget dashboard-container my-adslist col-lg-20 parent" >
+                    @if(count($products)>0)
+                        <table class="table table-responsive product-dashboard-table" style = "overflow: visible;" >
+                            <thead>
+                                <tr>
+                                    <th>image</th>
+                                    <th>Product </th>
+                                    <th class = "text-center">Price</th>
+                                    <th class = "text-center">Qty</th>
+                                    <th class = "text-center">SubTotal</th>
+                                    <th class = "text-center">Action</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                                @foreach(Auth::user()->carts->products as $product)
-                                    @if($product->pivot->ordered == null)
-                                        <tr>
-                                            <td class="product-thumb">
-                                                <a href="/products/{{$product->id}}"><img width="80px" height="auto" src="images/products/{{$product->id}}/1.jpg" alt="image description"></a>
-                                            </td>
-                                            <td class="product-details">
-                                                <a href="/products/{{$product->id}}"><h3 class="title">{{$product->product_name}}</h3></a>
-                                                @if($product->brand)
-                                                    <span class="location"><strong>Brand:</strong>{{$product->brand}}</span>
-                                                @endif
-                                                @if($product->color)
-                                                    <span class="add-id"><strong>Color:</strong>{{$product->color}}</span>
-                                                @else
-                                                    <span><strong>Posted on: </strong><time>{{$product->created_at}}</time> </span>
-                                                @endif
-                                                
-                                            </td>
-                                            <td class="product-category text-center">
-                                                <span class="categories">{{number_format($product->price)}}</span>
-                                            </td>
-                                            <td class="product-category">
-                                                <span class="categories" >
-                                                    <select name = "category_{{$product->id}}" id = "inputGroupSelect_{{$product->id}}" class="w-1" onchange="subtotal(this, {{$product->id}}, {{$product->price}})">
-                                                        @for ($i = 1; $i <= $product->quantity; $i++)
-                                                            <option value="{{$i}}" @if($i == ($product->pivot->quantity)) selected @endif >{{$i}}</option>  
-                                                            @if($i == 5)
-                                                                @break
-                                                            @endif  
-                                                        @endfor
-                                                            @if($product->quantity > 5)
-                                                                <option value="{{$product->quantity}}" @if($product->quantity == ($product->pivot->quantity)) selected @endif >{{$product->quantity}}</option>
-                                                            @endif
-                                                    </select>            
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    @foreach(Auth::user()->carts->products as $product)
+                                        @if($product->pivot->ordered == null)
+                                            <tr>
+                                                <td class="product-thumb">
+                                                    <a href="/products/{{$product->id}}"><img width="80px" height="auto" src="images/products/{{$product->id}}/1.jpg" alt="image description"></a>
+                                                </td>
+                                                <td class="product-details">
+                                                    <a href="/products/{{$product->id}}"><h3 class="title">{{$product->product_name}}</h3></a>
+                                                    @if($product->brand)
+                                                        <span class="location"><strong>Brand:</strong>{{$product->brand}}</span>
+                                                    @endif
+                                                    @if($product->color)
+                                                        <span class="add-id"><strong>Color:</strong>{{$product->color}}</span>
+                                                    @else
+                                                        <span><strong>Posted on: </strong><time>{{$product->created_at}}</time> </span>
+                                                    @endif
+                                                    
+                                                </td>
+                                                <td class="product-category text-center">
+                                                    <span class="categories">{{number_format($product->price)}}</span>
+                                                </td>
+                                                <td class="product-category">
+                                                    <span class="categories" >
+                                                        <select name = "category_{{$product->id}}" id = "inputGroupSelect_{{$product->id}}" class="w-1" onchange="subtotal(this, {{$product->id}}, {{$product->price}})">
+                                                            @for ($i = 1; $i <= $product->quantity; $i++)
+                                                                <option value="{{$i}}" @if($i == ($product->pivot->quantity)) selected @endif >{{$i}}</option>  
+                                                                @if($i == 5)
+                                                                    @break
+                                                                @endif  
+                                                            @endfor
+                                                                @if($product->quantity > 5)
+                                                                    <option value="{{$product->quantity}}" @if($product->quantity == ($product->pivot->quantity)) selected @endif >{{$product->quantity}}</option>
+                                                                @endif
+                                                        </select>            
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                <span class="categories" style = "position:relative; left:12px;" name = "sub-total" id= "subtotal_{{$product->id}}">
+                                                    @if($product->pivot->quantity == null)
+                                                        {{number_format($product->price)}}
+                                                    @else
+                                                        {{number_format($product->price * $product->pivot->quantity)}}
+                                                    @endif
                                                 </span>
-                                            </td>
-                                            <td>
-                                            <span class="categories" style = "position:relative; left:12px;" name = "sub-total" id= "subtotal_{{$product->id}}">
-                                                @if($product->pivot->quantity == null)
-                                                    {{number_format($product->price)}}
-                                                @else
-                                                    {{number_format($product->price * $product->pivot->quantity)}}
-                                                @endif
-                                            </span>
-                                            </td>
-                                            
-                                            <td class="action" data-title="Action">
-                                                <div class="">
-                                                    <ul class="list-inline justify-content-center">
-                                                        <li class="list-inline-item">
-                                                            <a class="delete" data-toggle="tooltip" data-placement="top" title="Delete from cart" href ="/cart/{{$product->id}}/delete">
-                                                                <i class="fa fa-trash"></i>
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endif
-                                @endforeach
+                                                </td>
+                                                
+                                                <td class="action" data-title="Action">
+                                                    <div class="">
+                                                        <ul class="list-inline justify-content-center">
+                                                            <li class="list-inline-item">
+                                                                <a class="delete" data-toggle="tooltip" data-placement="top" title="Delete from cart" href ="/cart/{{$product->id}}/delete">
+                                                                    <i class="fa fa-trash"></i>
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
                             </tbody> 
-                            @else
-                                 <tr><td  style="position:relative; left:300px;">
-                                        <i class="fa fa-trash fa-5x" style="position:relative; left:60px;"></i>
-                                        <h1>Empty Cart</h1>
-                                        @if (session('status'))
-                                            <h2 style="color :red;">{{ session('status') }}</h2>
-                                        @endif
-                                        @if (session('status_placed_order'))
-                                            <h2 style="color :red; position:relative; left:-80px;">{{ session('status_placed_order') }}</h2>
-                                        @endif
-                                        <a href = "/home" style="position:relative; left:-20px;">Click Here to Continue Shopping</a>
-                                    </td></tr>  
+                        </table>
+                    @else   
+                    <div class = "divEl">                     
+                            <i class="fa fa-trash fa-5x" ></i>
+                            <!--<h1>Empty Cart</h1>
+                            @if (session('status'))
+                                <h2 style="color :red;">{{ session('status') }}</h2>
                             @endif
-                    </table>
+                            @if (session('status_placed_order'))
+                                <h2 style="color :red;">{{ session('status_placed_order') }}</h2>
+                            @endif
+                            <a href = "/home">Click Here to Continue Shopping</a>-->
+                        </div> 
+                    @endif
                 </div>
             </div>
 
